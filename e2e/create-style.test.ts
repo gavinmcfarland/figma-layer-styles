cb7c8570f3ab7fbaa44c87acbde2b246c16ec208
Gavin McFarland <5551+gavinmcfarland@users.noreply.github.com>
2025-04-29 22:44:08 +0100

    Update package-lock.json to reflect new version of picomatch and add funding information. Modify playwright.config.ts to streamline command syntax. Refactor create-style test to simplify selection handling and comment out unnecessary click action.

--- e2e/create-style.test.ts
+++ e2e/create-style.test.ts
@@ -1,21 +1,14 @@
 import { test, expect } from 'plugma/playwright'
 
-function clearPage() {
-	const rects = figma.currentPage.children
-	rects.forEach((rect) => {
-		rect.remove()
-	})
-}
-
 test('create style', async ({ ui, main }) => {
 	await ui.goto('http://localhost:4000/')
 
 	await main(() => {
 		let pageChildren = figma.currentPage.children
-		figma.currentPage.selection = [pageChildren[0]]
+		figma.currentPage.selection = pageChildren
 	})
 
-	await ui.locator('#listItem0').click()
+	// await ui.locator('#listItem0').click()
 
 	// TODO: Update to use id when button supports id attribute
 	await ui.getByRole('button', { name: 'aria-label' }).click()
